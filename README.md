57a396dca4d0d6a6adff122b950a65fa1fad787d
Hamza Daoud <hamzadaoud99@gmail.com>
2021-07-29 20:35:13 +0300

    Adding the script and README file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # scanPSUTschedule
+This script is very useful for any PSUT student in the registration time, you can provide a list of courses numbers and the script will scan the schedule to take a screenshot for each course, so you can browse the info for each course with no need to search for each course manually.
+
+## How to use it?
+It's simple, just provide the list of numbers separated by a comma inside the `courses.txt` file like this [20141,20231,22592,52592] and run the script, the results will be in the output file.
+
+## Requierments
+You must have python 3 with `selenium` installed and the webdriver according to your browser:
+    - *Firefox:* https://github.com/mozilla/geckodriver/releases/
+    - *Google chrome:* https://chromedriver.chromium.org/
+
+Enjoy and have a nice semester :)
---
+++ scanPSUTschedule.py
@@ -0,0 +1,56 @@
+from genericpath import isfile
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+
+def makeOutputDir():
+    if (os.path.isdir("./Output")):
+        pass
+    else:
+        os.mkdir("./Output")
+def nameScreenshot(num):
+    if(os.path.isfile("./Output/"+num+".png")):
+        os.remove("./Output/"+num+".png")
+    return "./Output/"+num+".png"
+def welcoming():
+    print("Welcome!\nPut the courses numbers that you want to check in file named 'courses.txt' at the same folder sperated by a comma.\nExample: 20141,20231,22592\nPress any key when the file is ready and let the magic happen :)")
+    input()
+    if(not os.path.isfile("./courses.txt")):
+        print("I can't find 'courses.txt', try again!")
+        exit()
+
+welcoming()
+makeOutputDir()
+coursesNo = open("courses.txt","r")
+driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
+driver.get("https://regnew.psut.edu.jo/ProposedCoursesPublic.aspx")
+
+yearBox = driver.find_element_by_xpath('//*[@id="ddlStudyYear"]/option[2]')
+yearBox.click()
+semBox = driver.find_element_by_xpath('//*[@id="ddlStudySemister"]/option[2]')
+semBox.click()
+numbers = coursesNo.readline().split(",")
+
+for number in numbers:
+    searchButton = driver.find_element_by_xpath('//*[@id="btnSearch"]')
+    courseNo = driver.find_element_by_xpath('//*[@id="tbCourseNo"]')
+    courseNo.clear()
+    courseNo.send_keys(number)
+    searchButton.click()
+    time.sleep(1)
+    timeout = 5
+    try:
+        element_present = EC.presence_of_element_located((By.ID, 'gvProposedCoursesSchedule'))
+        WebDriverWait(driver, timeout).until(element_present)
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        table = driver.find_element_by_xpath('//*[@id="gvProposedCoursesSchedule"]')
+        table.screenshot(nameScreenshot(number))
+    except TimeoutException:
+        print("Timed out waiting for page to load, try again!")
+driver.close()
+print("Done! check the output folder.")
